72fa47b46e517fc11c11b06eab01df584f175ae8
DanilUst <ustinov2015danil@yandex.ru>
2025-05-28 23:07:28 +0300

    Мой третий коммит

--- swagger_server/metrics.py
+++ swagger_server/metrics.py
@@ -1,10 +1,34 @@
 from prometheus_client import Counter, Gauge, Histogram
 
-CARS_ADDED_TOTAL = Counter('cars_added_total', 'Total cars added')
-CARS_ADDED_DETAILED = Counter('cars_added_detailed', 
-                              'Cars added by model', ['car_model'])
-CARS_DELETED = Counter('cars_deleted_total', 'Total cars deleted')
-CARS_UPDATED = Counter('cars_updated_total', 'Total cars updated')
-CARS_IN_DB = Gauge('cars_in_database', 
-                   'Current cars count')
-API_LATENCY = Histogram('seller_api_latency_seconds', 'API latency', ['method', 'endpoint'])
+
+CARS_ADDED_TOTAL = Counter(
+    'cars_added_total',
+    'Total cars added'
+)
+
+CARS_ADDED_DETAILED = Counter(
+    'cars_added_detailed',
+    'Cars added by model',
+    ['car_model']
+)
+
+CARS_DELETED = Counter(
+    'cars_deleted_total',
+    'Total cars deleted'
+)
+
+CARS_UPDATED = Counter(
+    'cars_updated_total',
+    'Total cars updated'
+)
+
+CARS_IN_DB = Gauge(
+    'cars_in_database',
+    'Current cars count'
+)
+
+API_LATENCY = Histogram(
+    'seller_api_latency_seconds',
+    'API latency',
+    ['method', 'endpoint']
+)
